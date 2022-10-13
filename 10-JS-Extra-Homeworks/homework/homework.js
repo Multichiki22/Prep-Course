60085307657a7a18daa016e7c204af35d2809aed
multichiki22 <myguelangel12@gmail.com>
2022-10-12 20:00:15 -0500

    Homework 10 fully tested

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,15 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  var salida = []
+  for (let clave in objeto){
+    let tempArray = []
+    tempArray[0] = clave
+    tempArray[1] = objeto[clave]
+    salida.push(tempArray)
+    tempArray = []
+  }
+  return salida
 }
 
 
@@ -18,6 +27,19 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  var salida = {
+  }
+  for (let i=0;i<string.length;i++){
+    if (salida.hasOwnProperty([string[i]])=== false) {
+      salida[string[i]] = 1
+    }else {
+      salida[string[i]] = salida[string[i]] + 1
+    }
+  }
+  return salida
+  //this problem could be also solved with a hashmap (maybe more efficient in terms of time complexity)
+// but i imagine that the intended solution it was using objects
+
 }
 
 
@@ -26,6 +48,16 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  var mayus = ""
+  var minus = ""
+  for (let i = 0;i<s.length;i++){
+    if (s[i] === s[i].toUpperCase()){
+      mayus = mayus +s[i]
+    }else{
+      minus = minus +s[i]
+    }
+  }
+  return (mayus + minus)
 }
 
 
@@ -35,6 +67,26 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+  var espejo = function(inicio ,final, string){
+    var palabraEspejada = ""
+    for (let i = final;i>=inicio;i--){
+      palabraEspejada = palabraEspejada + string[i]
+    }
+    return palabraEspejada
+  }
+  var salida = ""
+  var tempInicio = 0
+  for (let i = 0;i<=str.length;i++){
+    if (i == str.length){
+      salida  = salida + espejo(tempInicio, i-1,str)
+      tempInicio = i+1
+      break
+    }else if (str[i]== " "){
+      salida  = salida + espejo(tempInicio, i-1,str) + " "
+      tempInicio = i+1
+    }
+  }
+  return salida
 } 
 
 
@@ -43,13 +95,31 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  var numeroStr = numero.toString()
+  var numeroTapicua = numeroStr.split("").reverse().join("")
+    if (numeroTapicua === numeroStr){
+    return "Es capicua"
+  }else {
+    return "No es capicua"
+  }
 }
 
 
 function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
-  //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
+  //y devuelva la versión modificada o la misma cadena, en caso de NO contener dichas letras.
   //Escribe tu código aquí
+  var salida = ""
+  for (let i = 0;i<cadena.length;i++){
+    switch(cadena[i]){
+      case "a":continue
+      case "b":continue
+      case "c":continue
+      default:
+        salida = salida + cadena[i]
+    }
+  }
+  return salida
 }
 
 
@@ -57,7 +127,33 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+  var temp = new Map()
+  for (let i =0;i<arr.length;i++){
+    var lengWord = arr[i].length
+    if (temp.has(lengWord)== false){
+      temp.set(lengWord,[arr[i]])
+    }else{
+      var cambio = temp.get(lengWord)
+      cambio.push(arr[i])
+      temp.set(lengWord,cambio)
+    }
+  }
+  var cants = []
+  for (let indi of temp.keys()){
+    cants.push(indi)
+  }
+  cants.sort()
+  var salida = []
+  for(let j = 0;j<cants.length;j++){
+    var moment = cants[j]
+    var actu = temp.get(moment)
+    for (let i = 0; i<actu.length;i++){
+      salida.push(actu[i])
+    }
+  }
+  return salida
 }
+
 
 
 function buscoInterseccion(arreglo1, arreglo2){
@@ -66,6 +162,25 @@ function buscoInterseccion(arreglo1, arreglo2){
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
   //Escribe tu código aquí  
+  var salida = []
+  if (arreglo1.length>arreglo2.length){
+    for (let i = 0;i<arreglo1.length;i++){
+      for (let j = 0;j<arreglo2.length;j++){
+        if (arreglo1[i]==arreglo2[j]){
+          salida.push(arreglo2[j])
+        }
+      }
+    }
+  }else{
+    for (let i = 0;i<arreglo2.length;i++){
+      for (let j = 0;j<arreglo1.length;j++){
+        if (arreglo2[i]==arreglo1[j]){
+          salida.push(arreglo1[j])
+        }
+      }
+    }
+  }
+  return salida
 }
 
 
